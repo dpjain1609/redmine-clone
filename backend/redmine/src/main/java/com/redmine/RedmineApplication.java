ece0caaff399c2f845c525e50acfff8a8bde085f
dpjain <dpjain@iu.edu>
2024-09-15 11:05:47 +0530

    Setup
    Summary:
    -setup react and tailwindcss for the frontend
    -create a new spring boot application for the backend

---
+++ backend/redmine/src/main/java/com/redmine/RedmineApplication.java
@@ -0,0 +1,13 @@
+package com.redmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmineApplication.class, args);
+	}
+
+}
---
+++ backend/redmine/src/test/java/com/redmine/RedmineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
